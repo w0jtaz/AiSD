a2b2e311b655b060cac1c8fcc7188ce7e7ecebb3
w0jtaz <wojtaz@vp.pl>
2021-12-07 23:15:23 +0100

    update

--- lab5/main.py
+++ lab5/main.py
@@ -1,115 +1,137 @@
-from typing import Any, Callable
 import treelib
+from typing import Any, Callable
 
-
+# binarny węzeł drzewa przechowujący wartość oraz lewe i prawe dziecko.
 class BinaryNode:
     value: Any
     left_child: 'BinaryNode'
     right_child: 'BinaryNode'
 
+    # inicjalizator ustawia wartość bieżącego węzła
     def __init__(self, value=Any) -> None:
         self.left_child = None
         self.right_child = None
         self.value = value
 
+    # przesłonięcie metody w celu uzyskania wartości
     def __str__(self):
         return str(self.value)
 
-    def is_leaf(self) -> bool:
-        if (self.right_child is None) and (self.left_child is None):
+    # is_leaf() sprawdza czy węzeł jest liściem
+    def is_leaf(self):
+        if (self.right_child == None) and (self.left_child == None):
             return True
         else:
             return False
 
+    # add_left_child dodaje lewe dziecko bieżącego węzła jako nowy węzeł
     def add_left_child(self, value: Any):
         self.left_child = BinaryNode(value)
 
+    # add_right_child dodaje prawe dziecko bieżącego węzła jako nowy węzeł
     def add_right_child(self, value: Any):
         self.right_child = BinaryNode(value)
 
-    def traverse_in_order(self, visit: Callable[[Any], None]) -> None:
-        if self.left_child is not None:
+    # traverse_post_order wykonuje operację wstecznego przejscia po podrzędnych węzłach
+    def traverse_in_order(self, visit: Callable[[Any], None]):
+        if self.left_child != None:
             self.left_child.traverse_in_order(visit)
         visit(self)
-        if self.right_child is not None:
+        if self.right_child != None:
             self.right_child.traverse_in_order(visit)
 
+    # traverse_pre_order wykonuje operację wzdłużnego przejścia po podrzędnych węzłach
     def traverse_post_order(self, visit: Callable[[Any], None]):
-        if self.left_child is not None:
+        if self.left_child != None:
             self.left_child.traverse_post_order(visit)
-        if self.right_child is not None:
+        if self.right_child != None:
             self.right_child.traverse_post_order(visit)
         visit(self)
 
+    # traverse_pre_order wykonuje operację wzdłużnego przejścia po podrzędnych węzłach rozpoczynając od korzenia
     def traverse_pre_order(self, visit: Callable[[Any], None]):
         visit(self)
-        if self.left_child is not None:
+        if self.left_child != None:
             self.left_child.traverse_pre_order(visit)
-        if self.right_child is not None:
+        if self.right_child != None:
             self.right_child.traverse_pre_order(visit)
 
-
+# przechowuje strukturę drzewa binarnego
 class BinaryTree:
     root: BinaryNode
 
-    def __init__(self, value= Any) -> None:
+    # inicjalizator, root reprezentuje węzeł będący korzeniem
+    def __init__(self, value= Any):
         self.root = BinaryNode(value)
 
-    def traverse_in_order(self, visit: Callable[[Any], None]) -> None:
+    # traverse_in_order wykonuje operację poprzecznego przejścia po podrzędnych węzłach rozpoczynając od korzenia
+    def traverse_in_order(self, visit: Callable[[Any], None]):
         self.root.traverse_in_order(visit)
 
-    def traverse_post_order(self, visit: Callable[[Any], None]) -> None:
+    # traverse_post_order wykonuje operację wstecznego przejscia po podrzędnych węzłach rozpoczynając od korzenia
+    def traverse_post_order(self, visit: Callable[[Any], None]):
         self.root.traverse_post_order(visit)
 
-    def traverse_pre_order(self, visit: Callable[[Any], None]) -> None:
+    # traverse_pre_order wykonuje operację wzdłużnego przejścia po podrzędnych węzłach rozpoczynając od korzenia
+    def traverse_pre_order(self, visit: Callable[[Any], None]):
         self.root.traverse_pre_order(visit)
 
-
+    # wyświetla drzewo w formie graficznej
     def show(self) -> None:
         binary_tree = treelib.Tree()
         binary_tree.create_node(str(self.root.value), str(self.root.value))
 
-        def createShow(node: 'BinaryNode') -> None:
-            if node.left_child is not None:
+        def binary_children(node: 'BinaryNode'):
+            if node.left_child != None:
                 binary_tree.create_node(str(node.left_child.value), str(node.left_child.value), parent=str(node.value))
-            if node.right_child is not None:
+            if node.right_child != None:
                 binary_tree.create_node(str(node.right_child.value), str(node.right_child.value), parent=str(node.value))
 
-        self.traverse_pre_order(createShow)
+        self.traverse_pre_order(binary_children)
         binary_tree.show()
 
-def closest_parent(tree: BinaryTree, first_node: BinaryNode, second_node: BinaryNode) -> BinaryNode:
-    if tree is first_node or tree is second_node:
-        return tree
-    elif tree is None:
+# zwraca najbliższy wspólny węzeł nadrzędny przekazanych węzłów first_node->n1 , second_node ->n2
+def closest_parent(binary_tree: BinaryTree, n1: BinaryNode, n2: BinaryNode) -> BinaryNode:
+    if binary_tree == n1 or binary_tree == n2:
+        return binary_tree
+    elif binary_tree == None:
         return None
 
-    left = closest_parent(tree.left_child, first_node, second_node)
-    right = closest_parent(tree.right_child, first_node, second_node)
+    left = closest_parent(binary_tree.left_child, n1, n2)
+    right = closest_parent(binary_tree.right_child, n1, n2)
 
-    if left is not None:
+    if left != None and right != None:
+        return binary_tree
+    elif left != None:
         return left
-    elif right is not None and left is not None:
-        return tree
     else:
         return right
 
 
-
+# wartość w korzeniu drzewa wynosi 10
 binary_tree = BinaryTree(10)
-binary_tree.root.add_left_child(9)
+binary_tree.root.add_left_child(8)
 binary_tree.root.add_right_child(2)
 binary_tree.root.left_child.add_left_child(1)
-binary_tree.root.left_child.add_right_child(4)
-binary_tree.root.right_child.add_left_child(3)
-binary_tree.root.right_child.add_right_child(6)
+binary_tree.root.left_child.add_right_child(5)
+binary_tree.root.right_child.add_left_child(4)
+binary_tree.root.right_child.add_right_child(7)
 
+# drukujemy drzewo na ekran
 binary_tree.show()
-print(closest_parent(binary_tree.root, binary_tree.root.left_child.right_child, binary_tree.root.left_child.left_child.left_child))
 
+# wyszukiwanie nadrzędnego węzła dla pary (2,8)
+print("Closest parent of (2,8): ",closest_parent(binary_tree.root, binary_tree.root.right_child, binary_tree.root.left_child))
+
+# wyszukiwanie nadrzędnego węzła dla pary (1,5)
+print("Closest parent of (1,5): ",closest_parent(binary_tree.root, binary_tree.root.left_child.left_child, binary_tree.root.left_child.right_child))
+
+# wyszukiwanie nadrzędnego węzła dla pary (4,7)
+print("Closest parent of (4,7): ",closest_parent(binary_tree.root, binary_tree.root.right_child.left_child, binary_tree.root.right_child.right_child))
+
+# testy poprawności implementacji
 assert binary_tree.root.value == 10
 assert binary_tree.root.right_child.value == 2
 assert binary_tree.root.right_child.is_leaf() is False
 assert binary_tree.root.left_child.left_child.value == 1
 assert binary_tree.root.left_child.left_child.is_leaf() is True
-
